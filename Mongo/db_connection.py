f14677e225c3bf0066aa33652ae87a33ffa46527
Pzzzi <shengjun2002@gmail.com>
2025-02-25 15:55:51 +0800

    Mongo

---
+++ Mongo/db_connection.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+# Replace with your MongoDB Atlas connection string
+MONGO_URI = "mongodb+srv://Jason:jason96493490@cluster0.e3lxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(MONGO_URI)
+db = client["news_db"]
+collection = db["news"]
+
+# Test connection
+print("Connected to MongoDB Atlas successfully!")
