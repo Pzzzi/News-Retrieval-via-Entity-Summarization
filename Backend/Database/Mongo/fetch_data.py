9e304570adb9f685069da2da2d5ae713d8d507fe
Pzzzi <shengjun2002@gmail.com>
2025-03-29 14:20:02 +0800

    temp change

--- Backend/Database/Mongo/fetch_data.py
+++
@@ -1,6 +0,0 @@
-from db_connection import collection  # Import collection from db_connection.py
-
-# Fetch all articles
-articles = collection.find()
-for article in articles:
-    print(f"{article['title']} - {article['date']}")
